2adeef031021c27757932d6e4dd9a8facfae7a5d
Lior Ben-David <liorbd@Outlook.com>
2021-06-18 18:56:13 -0400

    Commented

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+// This is a basic express server to serve our static html file
+
 var express = require('express');
 var app = express();
 var path = require('path');
+
 app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
